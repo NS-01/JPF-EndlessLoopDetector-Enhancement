992435cd899524389a0c3e3689cf963abc685446
NS-01 <nskk01@my.yorku.ca>
2018-03-24 23:25:57 -0400

    resolving conflict

--- jpf-core/src/main/gov/nasa/jpf/listener/EndlessLoopDetector.java
+++ jpf-core/src/main/gov/nasa/jpf/listener/EndlessLoopDetector.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and 
  * limitations under the License.
- */
+ 
 
 package gov.nasa.jpf.listener;
 
@@ -35,12 +35,12 @@ import gov.nasa.jpf.vm.MethodInfo;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.VM;
 
-/**
+*//**
  * little listener that tries to detect endless while() loops by counting
  * backjumps, breaking transitions if the count exceeds a threshold, and then
  * checking if program states match. If they do, there would be no progress in
  * this thread.
- */
+ *//*
 @JPFOptions({
 		@JPFOption(type = "Long", key = "endlessloopdetector.max_loops", defaultValue = "500", comment = "stop search after specified number of loops have occurred") })
 public class EndlessLoopDetector extends IdleFilter {
@@ -62,6 +62,206 @@ public class EndlessLoopDetector extends IdleFilter {
 		maxTime = config.getDuration("endlessloopdetector.max_loops", 500);
 		this.information = new ArrayList<String>();
 		action = Action.BREAK;
+	}
+
+	@Override
+	public void stateAdvanced(Search search) {
+		if (brokeTransition && search.isVisitedState()) {
+			foundEndlessLoop = true;
+		}
+	}
+
+	*//**
+	 * Whenever JPF enters a method, checks if it is the main method.
+	 *
+	 * @param vm
+	 *            JPF's virtual machine.
+	 * @param thread
+	 *            the thread currently executing.
+	 * @param method
+	 *            the method that is entered.
+	 *//*
+	@Override
+	public void methodEntered(VM vm, ThreadInfo thread, MethodInfo method) {
+		if (method.getFullName().contains("main([Ljava/lang/String;)V")) {
+			this.inMain = true;
+		}
+	}
+
+	*//**
+	 * Whenever JPF is executing an instruction, checks if the instruction belongs
+	 * to the main method. Keeps count of the number of times a method is called.
+	 * 
+	 * @param vm
+	 *            JPF's virtual machine.
+	 * @param thread
+	 *            the thread currently executing.
+	 * @param instruction
+	 *            the instruction that is executing.
+	 *//*
+	@Override
+	public void executeInstruction(VM vm, ThreadInfo thread, Instruction instruction) {
+		while (this.inMain && !foundEndlessLoop) {
+			String currentMethodName = instruction.getMethodInfo().getName();
+			if (this.methodCalls.containsKey(currentMethodName)) {
+				// update count
+				if (this.methodCalls.get(currentMethodName) >= this.maxTime) {
+					foundEndlessLoop = true;
+				} else {
+					this.methodCalls.put(currentMethodName, this.methodCalls.get(currentMethodName) + 1);
+				}
+			} else {
+				// add count = 1
+				this.methodCalls.put(currentMethodName, 1);
+			}
+		}
+			if (this.checkFinished(instruction)) {
+				// stop
+				// have to find a better way to do this.
+				//vm.getSearch().terminate();
+				thread.breakTransition(true);
+				//vm.terminateProcess(thread);
+			}
+			//vm.getSearch().p
+			//ConsolePublisher.printStatistics(console, reporter);
+		//}
+		//thread.breakTransition(true);
+	//	if (this.checkFinished(instruction)) {
+			// stop
+			// have to find a better way to do this.
+			//vm.getSearch().terminate();
+			//thread.breakTransition(true);
+			//vm.terminateProcess(thread);
+	//	}
+	}
+	
+	*//**
+	 * Whenever JPF exits a method, checks if it is the main method.
+	 *
+	 * @param vm
+	 *            JPF's virtual machine.
+	 * @param thread
+	 *            the thread currently executing.
+	 * @param method
+	 *            the method that is entered.
+	 *//*
+	@Override
+	public void methodExited(VM vm, ThreadInfo thread, MethodInfo method) {
+		if (method.getFullName().contains("main([Ljava/lang/String;)V")) {
+			this.inMain = false;
+		}
+	}
+
+	*//**
+	 * Verifies if an endless loop is found
+	 *//*
+	private boolean checkFinished(Instruction instruction) {
+		if (this.foundEndlessLoop) {
+			this.information
+					.add("Infinite Recursion detected at Line Number : " + String.valueOf(instruction.getLineNumber()));
+			this.printInformation();
+			return true;
+		}
+		return false;
+	}
+
+	*//**
+	 * Prints an error message when an endless loop is found
+	 *//*
+	private void printInformation() {
+		for (String s : this.information) {
+			System.out.println(s);
+		}
+	}
+
+	@Override
+	public boolean check(Search search, VM vm) {
+		return !foundEndlessLoop;
+	}
+
+	@Override
+	public void reset() {
+		foundEndlessLoop = false;
+	}
+
+}
+*/
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package gov.nasa.jpf.listener;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.annotation.JPFOption;
+import gov.nasa.jpf.annotation.JPFOptions;
+import gov.nasa.jpf.search.Search;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.MethodInfo;
+import gov.nasa.jpf.vm.ThreadInfo;
+import gov.nasa.jpf.vm.VM;
+
+/**
+ * Configuration : endlessloopdetector.max_loops - allows user to set maximum
+ * number of times a method can execute before considering current execution an
+ * infinitely recursive method, thereby an endless loop
+ */
+@JPFOptions({
+		@JPFOption(type = "Long", key = "endlessloopdetector.max_loops", defaultValue = "500", comment = "stop search after specified number of loops have occurred") })
+
+/**
+ * Configuration : idle.max_backjumps (found in
+ * gov.nasa.jpf.listener.IdleFilter) - allows user to set the maximum number of
+ * backjumps that can occur before considering current execution an endless loop
+ */
+
+/**
+ * little listener that tries to detect endless while() loops by counting
+ * backjumps, breaking transitions if the count exceeds a threshold, and then
+ * checking if program states match. If they do, there would be no progress in
+ * this thread.
+ */
+public class EndlessLoopDetector extends IdleFilter {
+
+	// information to print if endlessloop is detected
+	private List<String> information;
+
+	// HashTable key -> method name , value -> count
+	private Hashtable<String, Integer> methodCalls = new Hashtable<String, Integer>();
+
+	// max time set by configuration
+	private long maxTime;
+
+	// checker to identify endless loop detection
+	private boolean foundEndlessLoop = false;
+
+	// checker to determine if the current execution is in the main method
+	private boolean inMain;
+
+	// Constructor
+	public EndlessLoopDetector(Config config) {
+		super(config);
+		// default times recursion can occur before considering infinite is 500
+		this.maxTime = config.getDuration("endlessloopdetector.max_loops", 500);
+		this.information = new ArrayList<String>();
 	}
 
 	@Override
@@ -101,64 +301,38 @@ public class EndlessLoopDetector extends IdleFilter {
 	 */
 	@Override
 	public void executeInstruction(VM vm, ThreadInfo thread, Instruction instruction) {
-		while (this.inMain && !foundEndlessLoop) {
-			String currentMethodName = instruction.getMethodInfo().getName();
-			if (this.methodCalls.containsKey(currentMethodName)) {
-				// update count
-				if (this.methodCalls.get(currentMethodName) >= this.maxTime) {
-					foundEndlessLoop = true;
+		if (this.inMain) {
+			while (!this.foundEndlessLoop) {
+				String currentMethodName = instruction.getMethodInfo().getName();
+				if (this.methodCalls.containsKey(currentMethodName)) {
+					// update count
+					if (this.methodCalls.get(currentMethodName) >= this.maxTime) {
+						// infinite loop detected
+						foundEndlessLoop = true;
+					} else {
+						this.methodCalls.put(currentMethodName, this.methodCalls.get(currentMethodName) + 1);
+					}
 				} else {
-					this.methodCalls.put(currentMethodName, this.methodCalls.get(currentMethodName) + 1);
+					// add count = 1
+					this.methodCalls.put(currentMethodName, 1);
 				}
-			} else {
-				// add count = 1
-				this.methodCalls.put(currentMethodName, 1);
+
+				if (checkFinished()) {
+					vm.getSearch().notifySearchProbed();// -- required for randomsearch strategy
+					break;
+				}
+
 			}
-		}
-			if (this.checkFinished(instruction)) {
-				// stop
-				// have to find a better way to do this.
-				//vm.getSearch().terminate();
-				thread.breakTransition(true);
-				//vm.terminateProcess(thread);
-			}
-			//vm.getSearch().p
-			//ConsolePublisher.printStatistics(console, reporter);
-		//}
-		//thread.breakTransition(true);
-	//	if (this.checkFinished(instruction)) {
-			// stop
-			// have to find a better way to do this.
-			//vm.getSearch().terminate();
-			//thread.breakTransition(true);
-			//vm.terminateProcess(thread);
-	//	}
-	}
-	
-	/**
-	 * Whenever JPF exits a method, checks if it is the main method.
-	 *
-	 * @param vm
-	 *            JPF's virtual machine.
-	 * @param thread
-	 *            the thread currently executing.
-	 * @param method
-	 *            the method that is entered.
-	 */
-	@Override
-	public void methodExited(VM vm, ThreadInfo thread, MethodInfo method) {
-		if (method.getFullName().contains("main([Ljava/lang/String;)V")) {
-			this.inMain = false;
+			thread.breakTransition("EndlessLoop Detected - Infinite Recursion");
 		}
 	}
 
 	/**
 	 * Verifies if an endless loop is found
 	 */
-	private boolean checkFinished(Instruction instruction) {
+	private boolean checkFinished() {
 		if (this.foundEndlessLoop) {
-			this.information
-					.add("Infinite Recursion detected at Line Number : " + String.valueOf(instruction.getLineNumber()));
+			this.information.add("Endless Loop has been detected - Infinite Recursion");
 			this.printInformation();
 			return true;
 		}
