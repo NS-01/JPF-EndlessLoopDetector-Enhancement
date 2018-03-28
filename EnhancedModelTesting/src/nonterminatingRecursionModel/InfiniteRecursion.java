9bfd5f83907861911091766be65eb9eedf8808dd
Varsha <varsha.ragavendran@gmail.com>
2018-03-28 00:04:06 -0400

    addition of enhanced model testing classes
    - included a few test cases (those with endless loops, and those with
    guaranteed termination)
    - testing package code does not work.

---
+++ EnhancedModelTesting/src/nonterminatingRecursionModel/InfiniteRecursion.java
@@ -0,0 +1,29 @@
+package nonterminatingRecursionModel;
+
+/**
+ * Recursive method executed endlessly
+ * 
+ * @author Varsha
+ *
+ */
+public class InfiniteRecursion {
+	/**
+	 * Main method used for executing/verifying jpf config file
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		InfiniteRecursion infiniteRecursion = new InfiniteRecursion();
+		infiniteRecursion.recursiveMethod();
+	}
+
+	/**
+	 * Method recursively calls itself
+	 */
+	public void recursiveMethod() {
+		/*
+		 * for(int i = 0; i < 2 ; i++) { recursiveMethod(); }
+		 */
+		recursiveMethod();
+	}
+}
---
+++ EnhancedModelTesting/src/terminatingModel/SearchLoop.java
@@ -0,0 +1,40 @@
+package terminatingModel;
+
+import java.util.Random;
+
+/**
+ * Endless random search class that does not terminate the search.
+ * 
+ * @author Varsha
+ *
+ */
+public class SearchLoop {
+
+	public static void main(String[] args) {
+		SearchLoop searchLoop = new SearchLoop();
+		searchLoop.search();
+	}
+
+	/**
+	 * Endless random search
+	 */
+	public void search() {
+		Random random = new Random();
+		System.out.println("0");
+		if (random.nextBoolean()) {
+			System.out.println("2");
+		} else {
+			System.out.println("1");
+			if (random.nextBoolean()) {
+				System.out.println("4");
+			} else {
+				System.out.println("3");
+				if (random.nextBoolean()) {
+					System.out.println("6");
+				} else {
+					System.out.println("5");
+				}
+			}
+		}
+	}
+}
